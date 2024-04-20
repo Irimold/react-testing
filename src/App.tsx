7ec82673c6cfd3900c2172be2cb57d3a8d9d01d2
Levine Lighto <lighto.cc2@gmail.com>
2024-04-20 16:40:19 +0800

    fix: Dependency & page

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Button } from '@irimold/react-component'
+import "@irimold/react-component/dist/style.css"
 import './App.css'
+import { FilledAssets } from '@irimold/react-icons'
+import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Irimold React Testing Ground</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test Hot Module Refresh
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <div className="text-center">
+                Count: { count }
+            </div>
+            <Button
+                leftIcon={FilledAssets}
+                onClick={() => setCount(count + 1)}
+            >
+                Test button
+            </Button>
+        </>
+    )
 }
 
 export default App
